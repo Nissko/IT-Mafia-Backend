c73d11950ba9238a08a253cc96e4ba4d7682caa2
Nissko <86054392+Nissko@users.noreply.github.com>
2023-12-13 01:03:13 +0500

    rework project

--- BackendMafia/Program.cs
+++ BackendMafia/Program.cs
@@ -1,28 +1,26 @@
+using BackendMafia.Applications.Gun.Queries;
+using Domain.Entities.ShopAggregate;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
+using UI.Applications.Gun.Commands.StoreGuns;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("ReactPolicy",
-        builder =>
-        {
-            builder.WithOrigins("http://localhost:3000") // Укажите URL вашего React-фронтенда
-                   .AllowAnyMethod()
-                   .AllowAnyHeader();
-        });
-});
-
 //RegisterDbContext
 builder.Services.AddDbContext<MafiaApiDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//RegisterMediatR Methods
+builder.Services.AddTransient<IRequestHandler<GetGunsQuery, IEnumerable<Gun>>, GetGunsQueryHandler>();
+builder.Services.AddTransient<IRequestHandler<StoreGunsCommand, Unit>, StoreGunsCommandHandler>();
 
 var app = builder.Build();
 
