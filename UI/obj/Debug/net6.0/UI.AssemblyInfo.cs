c43c2bbf281a06dc5bb8cc3dae805c505c4ae1a0
Nissko <86054392+Nissko@users.noreply.github.com>
2023-12-13 14:13:30 +0500

    test

--- UI/obj/Debug/net6.0/UI.AssemblyInfo.cs
+++ UI/obj/Debug/net6.0/UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f85fd44fd790e8d029a74787faa096de30c1b50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
