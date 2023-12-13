5bead45099cbbfcd75582ea438be3312801251ce
Nissko <86054392+Nissko@users.noreply.github.com>
2023-12-13 13:49:05 +0500

    Delete UI directory

--- UI/Applications/Gun/Queries/GetGunsQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-using Domain.Entities.ShopAggregate;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
-
-namespace BackendMafia.Applications.Gun.Queries
-{
-    public class GetGunsQuery : IRequest<IEnumerable<Domain.Entities.ShopAggregate.Gun>> { }
-}
